d68effaa31e5cfae91489a9582a6d3b7dfbee62e
RussiSunni <jonathandyason@hotmail.com>
2023-11-14 21:36:38 +0200

    asd

--- src/game/scenes/4_Level2/scene4_9.js
+++ src/game/scenes/4_Level2/scene4_9.js
@@ -27,13 +27,13 @@ export default class Scene4_9 extends Phaser.Scene {
         // Header.
         var header = this.add.graphics();
         header.fillStyle(0xffffff, 1);
-        header.fillRoundedRect(960 - 740, 150, 1520, 210, 72);
-        var headerText = this.add.text(960 - 740, 175,
+        header.fillRoundedRect(960 - 740, 150, 1520, 190, 72);
+        var headerText = this.add.text(960 - 740 + (1520 / 2), 240,
             `Fun Fact: Justin Bieber claims the record for selling out Madison Square
 Garden the fastest of any artist. Two shows for his Believe tour sold out in 30
 seconds! Who do you think held the record before Justin Bieber?`,
             { fontFamily: "Arial", fontSize: "84px", fill: "#000000", align: "center" });
-        headerText.setOrigin(0).setScale(0.5)
+        headerText.setOrigin(0.5).setScale(0.5)
         headerText.alpha = 1
 
         // Button 1.
@@ -44,10 +44,10 @@ seconds! Who do you think held the record before Justin Bieber?`,
         btn1Border.lineStyle(5, 0x87d1ff, 1);
         btn1Border.strokeRoundedRect(0, 0, 690, 120, 8);
         btn1Border.setAlpha(0)
-        var btn1Text = this.add.text(100, 60, "A. Taylor Swift", { fontFamily: "Arial", fontSize: "72px", fill: "#000000" });
-        btn1Text.setOrigin(0.5).setScale(0.5)
+        var btn1Text = this.add.text(50, 42, "A. Taylor Swift", { fontFamily: "Arial", fontSize: "72px", fill: "#000000" });
+        btn1Text.setOrigin(0).setScale(0.5)
         btn1Text.alpha = 1
-        var btn1 = this.add.container(960 - (690 / 2), 480, [btn1Graphic, btn1Text, btn1Border]);
+        var btn1 = this.add.container(960 - (690 / 2), 380, [btn1Graphic, btn1Text, btn1Border]);
         btn1.setInteractive(new Phaser.Geom.Rectangle(0, 0, 690, 120), Phaser.Geom.Rectangle.Contains);
         btn1.on('pointerover', function () {
             // Change mouse cursor.
@@ -74,10 +74,10 @@ seconds! Who do you think held the record before Justin Bieber?`,
         btn2Border.lineStyle(5, 0x87d1ff, 1);
         btn2Border.strokeRoundedRect(0, 0, 690, 120, 8);
         btn2Border.setAlpha(0)
-        var btn2Text = this.add.text(100, 60, "B. Drake", { fontFamily: "Arial", fontSize: "72px", fill: "#000000" });
-        btn2Text.setOrigin(0.5).setScale(0.5)
+        var btn2Text = this.add.text(50, 42, "B. Drake", { fontFamily: "Arial", fontSize: "72px", fill: "#000000" });
+        btn2Text.setOrigin(0).setScale(0.5)
         btn2Text.alpha = 1
-        var btn2 = this.add.container(960 - (690 / 2), 680, [btn2Graphic, btn2Text, btn2Border]);
+        var btn2 = this.add.container(960 - (690 / 2), 540, [btn2Graphic, btn2Text, btn2Border]);
         btn2.setInteractive(new Phaser.Geom.Rectangle(0, 0, 690, 120), Phaser.Geom.Rectangle.Contains);
         btn2.on('pointerover', function () {
             // Change mouse cursor.
@@ -90,11 +90,12 @@ seconds! Who do you think held the record before Justin Bieber?`,
             this.canvas.style.cursor = "default";
         });
         btn2.on('pointerdown', function () {
-            btn1Border.setAlpha(0)
-            btn3Border.setAlpha(0)
             btn2Border.setAlpha(1)
+            btn3Border.setAlpha(0)
+            btn1Border.setAlpha(0)
             isCorrect = false
         }, this);
+
 
         // Button 3.
         var btn3Graphic = this.add.graphics();
@@ -104,10 +105,10 @@ seconds! Who do you think held the record before Justin Bieber?`,
         btn3Border.lineStyle(5, 0x87d1ff, 1);
         btn3Border.strokeRoundedRect(0, 0, 690, 120, 8);
         btn3Border.setAlpha(0)
-        var btn3Text = this.add.text(100, 60, "C. Maroon 5", { fontFamily: "Arial", fontSize: "72px", fill: "#000000" });
-        btn3Text.setOrigin(0.5).setScale(0.5)
+        var btn3Text = this.add.text(50, 42, "C. Maroon 5", { fontFamily: "Arial", fontSize: "72px", fill: "#000000" });
+        btn3Text.setOrigin(0).setScale(0.5)
         btn3Text.alpha = 1
-        var btn3 = this.add.container(960 - (690 / 2), 880, [btn3Graphic, btn3Text, btn3Border]);
+        var btn3 = this.add.container(960 - (690 / 2), 700, [btn3Graphic, btn3Text, btn3Border]);
         btn3.setInteractive(new Phaser.Geom.Rectangle(0, 0, 690, 120), Phaser.Geom.Rectangle.Contains);
         btn3.on('pointerover', function () {
             // Change mouse cursor.
@@ -127,12 +128,9 @@ seconds! Who do you think held the record before Justin Bieber?`,
         }, this);
 
 
-
-
-
         // Submit button.
         this.nextBtnAudio = this.sound.add("next-button", { loop: false });
-        const submitBtn = new WideButton(this, 0, 0, 'Submit', this.nextBtnAudio);
+        const submitBtn = new WideButton(this, 0, 900, 'Submit', this.nextBtnAudio);
         submitBtn.on('pointerdown', function () {
             if (isCorrect) {
                 this.scene.start("Scene4_9_correct", { music: this.music });
@@ -142,8 +140,6 @@ seconds! Who do you think held the record before Justin Bieber?`,
             }
         }, this);
         submitBtn.x = 960 - 130
-        submitBtn.y = 1080 - 70
-        submitBtn.alpha = 1
 
 
         // Next button.
